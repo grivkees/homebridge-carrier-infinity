e4fb0fcd4572c6fc40c4efca18b2021f9f0a1b02
Glenn Rivkees <grivkees@users.noreply.github.com>
2021-08-02 00:28:02 -0400

    feat: show manual/auto selector on fan control in homekit
    fix #85

--- src/characteristics_fan.ts
+++ src/characteristics_fan.ts
@@ -3,15 +3,61 @@ import { ThermostatCharacteristicWrapper, MultiWrapper } from './base';
 import { convertCharFan2SystemFan, convertSystemFan2CharFan } from './helpers';
 import { FAN_MODE, SYSTEM_MODE } from './infinityApi';
 
+/*
+ * Controls for system fan.
+ *
+ * Fan is controlled using a slider with the following steps:
+ * 0. Auto (0%)
+ * 1. Always On Low (33%)
+ * 2. Always On Medium (66%)
+ * 3. Always On High (100%)
+ *
+ * HomeKit will also show a "Auto/Manual" slider, which will always reflect the
+ * setting above. Manual denotes an Always On option is active.
+ *
+ * If a user ever tries to control the fan when their system is OFF, the system
+ * will be changed to FAN_ONLY mode. To get out of FAN_ONLY mode, the user can
+ * change the thermostat to an ON mode (HEAT/COOL/AUTO).
+ */
+
 class FanStatus extends ThermostatCharacteristicWrapper {
   ctype = this.Characteristic.Active;
   get = async () => {
-    return await this.system.config.getZoneActivityFan(
-      this.context.zone,
-      await this.getActivity(),
-    ) === FAN_MODE.OFF ?
-      this.Characteristic.Active.INACTIVE :
-      this.Characteristic.Active.ACTIVE;
+    // if the fan is on ... the fan is on
+    if (await this.system.config.getMode() === SYSTEM_MODE.FAN_ONLY) {
+      return this.Characteristic.Active.ACTIVE;
+    }
+    // if the system is on and any fan override is set ... the fan must be on
+    if (
+      await this.system.config.getMode() !== SYSTEM_MODE.OFF &&
+      await this.system.config.getZoneActivityFan(this.context.zone, await this.getActivity()) !== FAN_MODE.OFF
+    ) {
+      return this.Characteristic.Active.ACTIVE;
+    }
+    // anything else ...
+    return this.Characteristic.Active.INACTIVE;
+  };
+
+  set = async (value: CharacteristicValue) => {
+    // if we are trying to *turn on* fan, and system is off, set to fan only mode
+    if (
+      value === this.Characteristic.Active.ACTIVE &&
+      await this.system.config.getMode() === SYSTEM_MODE.OFF
+    ) {
+      return await this.system.config.setMode(SYSTEM_MODE.FAN_ONLY);
+    }
+
+    // if we are trying to turn off fan, turn off fan override (i.e. set fan speed=auto)
+    // NOTE: If system is in FAN_ONLY mode, it will remain in FAN ONLY, but with speed=auto. 
+    if (value === this.Characteristic.Active.INACTIVE) {
+      return await this.system.config.setZoneActivity(
+        this.context.zone,
+        null,
+        null,
+        await this.getHoldTime(),
+        FAN_MODE.OFF,
+      );
+    }
   };
 }
 
@@ -26,19 +72,12 @@ class FanSpeed extends ThermostatCharacteristicWrapper {
   };
 
   set = async (value: CharacteristicValue) => {
-    // Make sure system mode is right for manual fan settings
-    if (
-      await this.system.config.getMode() === SYSTEM_MODE.OFF &&
-        convertCharFan2SystemFan(value) !== FAN_MODE.OFF
-    ) {
+    // if we are trying to control fan, and system is off, set to fan only mode
+    if (await this.system.config.getMode() === SYSTEM_MODE.OFF) {
       await this.system.config.setMode(SYSTEM_MODE.FAN_ONLY);
-    } else if (
-      await this.system.config.getMode() === SYSTEM_MODE.FAN_ONLY &&
-        convertCharFan2SystemFan(value) === FAN_MODE.OFF
-    ) {
-      await this.system.config.setMode(SYSTEM_MODE.OFF);
     }
-    // Set zone activity
+
+    // set fan speed
     return await this.system.config.setZoneActivity(
       this.context.zone,
       null,
@@ -47,12 +86,43 @@ class FanSpeed extends ThermostatCharacteristicWrapper {
       convertCharFan2SystemFan(value),
     );
   };
+}
 
+class TargetFanState extends ThermostatCharacteristicWrapper {
+  ctype = this.Characteristic.TargetFanState;
+
+  get = async () => {
+    return await this.system.config.getZoneActivityFan(
+      this.context.zone,
+      await this.getActivity(),
+    ) === FAN_MODE.OFF ?
+      this.Characteristic.TargetFanState.AUTO :
+      this.Characteristic.TargetFanState.MANUAL;
+  };
+
+  set = async (value: CharacteristicValue) => {
+    // if we are trying to control fan, and system is off, set to fan only mode
+    if (await this.system.config.getMode() === SYSTEM_MODE.OFF) {
+      await this.system.config.setMode(SYSTEM_MODE.FAN_ONLY);
+    }
+
+    // set fan speed to switch between auto and manual
+    return await this.system.config.setZoneActivity(
+      this.context.zone,
+      null,
+      null,
+      await this.getHoldTime(),
+      value === this.Characteristic.TargetFanState.AUTO ?
+        FAN_MODE.OFF :  // fan off is auto
+        FAN_MODE.MED, // moving to manual defaults to med
+    );
+  };
 }
 
 export class FanService extends MultiWrapper {
   WRAPPERS = [
     FanStatus,
     FanSpeed,
+    TargetFanState,
   ];
 }
