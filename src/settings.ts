cf53b27d98cb54e7fae20b8d7235857e96e6716a
Glenn Rivkees <grivkees@users.noreply.github.com>
2022-02-17 01:19:30 -0500

    chore: enable spell checking
    and fix my existing typos

--- src/settings.ts
+++ src/settings.ts
@@ -9,7 +9,7 @@ export const PLATFORM_NAME = 'CarrierInfinity';
 export const PLUGIN_NAME = 'homebridge-carrier-infinity';
 
 /**
- * Carrier/Bryant Infnity/Evolution API Settings
+ * Carrier/Bryant Infinity/Evolution API Settings
  */
 export const INFINITY_API_BASE_URL = 'https://www.app-api.ing.carrier.com';
 export const INFINITY_API_CONSUMER_KEY = '8j30j19aj103911h';
