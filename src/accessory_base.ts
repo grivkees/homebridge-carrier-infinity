1e97c21044f1dccc56f3fb9e1237a35920845a21
Glenn Rivkees <grivkees@users.noreply.github.com>
2022-06-28 03:28:41 -0400

    refactor(platform): create accessory base and move therm to it

---
+++ src/accessory_base.ts
@@ -0,0 +1,28 @@
+import { PlatformAccessory } from 'homebridge';
+import { CarrierInfinityHomebridgePlatform } from './platform';
+import { PLATFORM_NAME, PLUGIN_NAME } from './settings';
+
+export abstract class BaseAccessory {
+  public readonly accessory: PlatformAccessory;
+
+  constructor(
+    protected readonly platform: CarrierInfinityHomebridgePlatform,
+    context: Record<string, string>,
+  ) {
+    const uuid = this.platform.api.hap.uuid.generate(this.ID(context));
+    let accessory = this.platform.accessories.find(accessory => accessory.UUID === uuid);
+    if (!accessory) {
+      this.platform.log.info(`[${context.name}] Added`);
+      accessory = new this.platform.api.platformAccessory(context.name, uuid);
+      accessory.context = context;
+      this.platform.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
+    } else {
+      this.platform.log.info(`[${context.name}] Loaded`);
+      accessory.context = context;
+      this.platform.api.updatePlatformAccessories([accessory]);
+    }
+    this.accessory = accessory;
+  }
+
+  protected abstract ID(context: Record<string, string>): string;
+}
