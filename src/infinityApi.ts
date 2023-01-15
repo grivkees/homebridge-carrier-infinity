20daedad861c18f4208d651cd3360038334aa3b3
Glenn Rivkees <grivkees@users.noreply.github.com>
2023-01-14 22:37:59 -0500

    refactor(api): move shared rest client away from data model

--- src/infinityApi.ts
+++ src/infinityApi.ts
@@ -1,14 +1,12 @@
 import { processSetpointDeadband } from './helpers';
-import { INFINITY_API_BASE_URL } from './settings';
-import { OAuthHeaders } from './api/oauth';
-
-import Axios, { AxiosInstance } from 'axios';
 import { MemoizeExpiring } from 'typescript-memoize';
 import { Mutex, tryAcquire, E_ALREADY_LOCKED, E_CANCELED, E_TIMEOUT } from 'async-mutex';
 import * as xml2js from 'xml2js';
 import { Logger } from 'homebridge';
 import hash from 'object-hash';
 import { PrefixLogger } from './helper_logging';
+import { InfinityRestClient } from './api/rest_client';
+import Axios from 'axios';
 
 export const SYSTEM_MODE = {
   OFF: 'off',
@@ -75,117 +73,6 @@ interface Zone {
   damperposition?: string[];
 }
 
-export class InfinityEvolutionApiConnection {
-  private token = '';
-  public axios: AxiosInstance;
-
-  constructor(
-    public username: string,
-    private password: string,
-    public readonly log: Logger) {
-    this.axios = Axios.create({
-      baseURL: INFINITY_API_BASE_URL,
-      headers: {
-        featureset: 'CONSUMER_PORTAL',
-        Accept: 'application/xml',
-      },
-    });
-    // Axios debug logging and error handling
-    this.axios.interceptors.response.use(
-      // Success
-      response => {
-        this.log.debug(
-          `${response.request?.method} ${response.request.host}${response.request?.path}`,
-          `${response.status} ${response.statusText}`,
-        );
-        return response;
-      },
-      // Failure
-      error => {
-        if (Axios.isAxiosError(error)) {
-          this.log.debug(
-            `${error.request?.method} ${error.request?.host}${error.request?.path}`,
-            `${error.response?.status} ${error.response?.statusText}`,
-          );
-        }
-        return Promise.reject(error); // this makes http errors raise
-      },
-    );
-    // Oauth header add
-    this.axios.interceptors.request.use(config => {
-      return OAuthHeaders.intercept(config, this.username, this.token);
-    });
-  }
-
-  // Api seems to expect this every min or so. more frequent doesn't seem to
-  // make any difference.
-  @MemoizeExpiring(1 * 60 * 1000)
-  async activate(): Promise<void> {
-    try {
-      await this.forceActivate();
-    } catch (error) {
-      this.log.error(
-        '[API] Failure sending activation signal: ',
-        Axios.isAxiosError(error) ? error.message : error,
-      );
-    }
-  }
-
-  async forceActivate(): Promise<void> {
-    await this.axios.post(
-      `/users/${this.username}/activateSystems`,
-      null,
-      {
-        headers: {
-          Accept: 'application/json',
-        },
-      },
-    );
-  }
-
-  @MemoizeExpiring(24 * 60 * 60 * 1000) // every 24 hrs
-  async refreshToken(): Promise<void> {
-    try {
-      await this.forceRefreshToken();
-    } catch (error) {
-      this.log.error(
-        '[API] Could not refresh access token: ',
-        Axios.isAxiosError(error) ? error.message : error,
-      );
-    }
-  }
-
-  // TODO: on some api errors, force a refresh
-  async forceRefreshToken(): Promise<void> {
-    const builder = new xml2js.Builder({cdata: true, headless: true});
-    const new_xml = builder.buildObject({
-      credentials: {
-        username: this.username,
-        password: this.password,
-      },
-    });
-    const data = `data=${encodeURIComponent(new_xml)}`;
-
-    const response = await this.axios.post(
-      '/users/authenticated',
-      data,
-      {
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-          Accept: 'application/json',
-        },
-      },
-    );
-
-    if (response.data.result?.accessToken) {
-      this.token = response.data.result.accessToken;
-    } else {
-      this.log.debug(response.data);
-      throw new Error('User authentication error.');
-    }
-  }
-}
-
 abstract class BaseInfinityEvolutionApiModel {
   // TODO make unknown and handle type checking in get methods
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -196,7 +83,7 @@ abstract class BaseInfinityEvolutionApiModel {
   protected log: Logger = new PrefixLogger(this.api_connection.log, 'API');
 
   constructor(
-    protected readonly api_connection: InfinityEvolutionApiConnection,
+    protected readonly api_connection: InfinityRestClient,
   ) {
     this.write_lock = new Mutex();
   }
@@ -270,7 +157,7 @@ abstract class BaseInfinityEvolutionSystemApiModel extends BaseInfinityEvolution
   protected HASH_IGNORE_KEYS = new Set<string>(['timestamp', 'localTime']);
 
   constructor(
-    protected readonly api_connection: InfinityEvolutionApiConnection,
+    protected readonly api_connection: InfinityRestClient,
     public readonly serialNumber: string,
     protected readonly log: Logger,
   ) {
@@ -736,7 +623,7 @@ export class InfinityEvolutionSystemModel {
   public log: Logger = new PrefixLogger(this.api_connection.log, this.serialNumber);
 
   constructor(
-    protected readonly api_connection: InfinityEvolutionApiConnection,
+    protected readonly api_connection: InfinityRestClient,
     public readonly serialNumber: string,
   ) {
     const api_logger = new PrefixLogger(this.log, 'API');
